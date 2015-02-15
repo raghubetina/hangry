522445fa48ad494a453b88b93a27042bc021af72
Raghu Betina <rbetina@github.com>
2015-02-15 17:58:38 -0600

    Adds validations, associations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,9 @@
 class Restaurant < ActiveRecord::Base
+  validates :name, :presence => true, :uniqueness => { :scope => :address }
+  validates :address, :presence => true
+  validates :phone_number, :presence => true
+  validates :opens_at, :presence => true
+  validates :closes_at, :presence => true
+
+  has_many :wait_times
 end
--- app/models/wait_time.rb
+++ app/models/wait_time.rb
@@ -1,3 +1,8 @@
 class WaitTime < ActiveRecord::Base
+  validates :restaurant, :presence => true
+  validates :party_size, :presence => true
+  validates :minutes, :presence => true
+  validates :checked_at, :presence => true
+
   belongs_to :restaurant
 end
